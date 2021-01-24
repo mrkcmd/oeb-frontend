54b691d385f6426b509f15db4a096829b9752579
mark chin <mrkcmd@gmail.com>
2021-01-24 14:00:54 +0700

    fix delay download

--- src/views/MyEbook.vue
+++ src/views/MyEbook.vue
@@ -246,7 +246,7 @@ export default {
             this.resetTimer();
             this.isDownloading = false;
             this.refreshList();
-          }, 500);
+          }, 1000);
         })
         .catch(error => {
           console.log(error);
