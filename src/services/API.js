a34ea8fd37a7acc8077ba919f9af0295c398ea82
mark chin <mrkcmd@gmail.com>
2021-02-07 23:47:45 +0700

    change backend url

--- src/services/API.js
+++ src/services/API.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default () => {
   return axios.create({
-    baseURL: "https://pdx-ebook.herokuapp.com/"
+    baseURL: "https://ebook-taksaonline-backend.herokuapp.com/"
   });
 };
